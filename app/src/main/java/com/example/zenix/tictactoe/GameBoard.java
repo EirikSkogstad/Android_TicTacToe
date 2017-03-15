e9a043827901ec91871161cdbc08033b40a3946e
Zenix <ze9ix@live.no>
2017-03-15 12:44:34 +0100

    Work on gameBoard. Extracted String resources

--- app/src/main/java/com/example/zenix/tictactoe/GameBoard.java
+++ app/src/main/java/com/example/zenix/tictactoe/GameBoard.java
@@ -5,9 +5,28 @@ import android.os.Bundle;
 
 public class GameBoard extends AppCompatActivity {
 
+    private String playerOneName;
+    private String playerTwoName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
+
+        handleActivity();
+        initComponents();
+        initListeners();
+    }
+
+    private void handleActivity() {
+
+    }
+
+    private void initComponents() {
+
+    }
+
+    private void initListeners() {
+
     }
 }

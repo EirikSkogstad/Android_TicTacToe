d59d8ca0782ca0518b444d045a642c94a013eedb
Eirik <Skogeir15@student.westerdals.no>
2017-03-16 09:55:38 +0100

    Test class skeleton

---
+++ app/src/test/java/com/example/zenix/tictactoe/FileHandlerTest.java
@@ -0,0 +1,41 @@
+package com.example.zenix.tictactoe;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileHandlerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+    @Test
+    public void testWriteHighScore() {
+
+    }
+
+    @Test
+    public void testWriteHighScore_OverwritingExistingEntries() {
+
+    }
+
+    @Test
+    public void testReadHighScore() {
+
+    }
+
+    @Test
+    public void testReadHighScore_NonExistentEntry() {
+
+    }
+
+
+
+}

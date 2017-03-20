09e9cc6ff30c2bb6bc932ec240e003db5c20991b
Eirik <Skogeir15@student.westerdals.no>
2017-03-20 19:35:28 +0100

    More initialization work

--- app/src/main/java/com/example/zenix/tictactoe/GameBoard.java
+++ app/src/main/java/com/example/zenix/tictactoe/GameBoard.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class GameBoard {
 
+    private GameSymbol currentPlayer;
     private ArrayList<GameSymbol> board;
     private ArrayList<GameRow> rows;
 
@@ -17,6 +18,34 @@ public class GameBoard {
             board.add(GameSymbol.EMPTY);
         }
 
+        // Horizontal
+        rows.add(new GameRow(board.get(0), board.get(1), board.get(2)));
+        rows.add(new GameRow(board.get(3), board.get(4), board.get(5)));
+        rows.add(new GameRow(board.get(6), board.get(7), board.get(8)));
+
+        // Vertical
+        rows.add(new GameRow(board.get(0), board.get(3), board.get(6)));
+        rows.add(new GameRow(board.get(1), board.get(4), board.get(7)));
+        rows.add(new GameRow(board.get(2), board.get(5), board.get(8)));
+
+        // Diagonal
+        rows.add(new GameRow(board.get(0), board.get(4), board.get(8)));
+        rows.add(new GameRow(board.get(2), board.get(4), board.get(6)));
+    }
+
+    public void updateBoard(int index) {
+        board.set(index, currentPlayer);
+
+        updateCurrentPlayer();
+    }
+
+    private void updateCurrentPlayer() {
+        if (currentPlayer == GameSymbol.X) {
+            currentPlayer = GameSymbol.O;
+        }
+        else {
+            currentPlayer = GameSymbol.X;
+        }
     }
 
 
